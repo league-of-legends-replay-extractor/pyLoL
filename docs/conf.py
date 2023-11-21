46e82ac6f134aa6e4eed1314c9736ec74fcbb488
sykim1106 <53938323+kimsy1106@users.noreply.github.com>
2023-11-21 14:24:49 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -56,7 +57,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = [sphinx_rtd_theme.get_html_theme_path()] #"sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
